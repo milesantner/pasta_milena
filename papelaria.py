13cecc284baf3f84220404bbfa08cc68580bac6e
Milena Santner <santnermi543@gmail.com>
2025-08-09 10:15:35 -0300

    Add files via upload

---
+++ papelaria.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import tkinter as tk
+from tkinter import messagebox
+
+lista = []
+
+def adicionar_produtos():
+    produto = add_produto.get()
+    categoria = add_categoria.get()
+    quantidade = add_quantidade.get()
+    valor = add_valor.get()
+
